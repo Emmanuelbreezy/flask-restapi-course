ce1552a1ebccfe542081facdc4860be97e1203cb
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2022-10-15 16:33:31 +0100

    first commit

--- resources/item.py
+++ resources/item.py
@@ -1,28 +1,27 @@
-from pydoc import describe
 import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import ItemSchema,ItemUpdateSchema
 
 blp = Blueprint("items", __name__, description="Operations on stores")
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
-    def get(self, store_id):
+    
+    @blp.response(200, ItemSchema)
+    def get(self, item_id):
         try:
             return items[item_id]
         except KeyError:
             abort(404, message="Item not found")
-    
-    def put(self, store_id):
-        item_data = request.get_json()
-        if(
-            "price" not in item_data
-            or "name" not in item_data
-        ):
-            abort(404, message="All fields are required")
+            
+            
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemUpdateSchema)
+    def put(self, item_data):
         try:
             item = items[item_id]
             item |= item_data
@@ -42,26 +41,19 @@ class Item(MethodView):
             
 @blp.route("/item")
 class ItemList(MethodView):
+    
+    @blp.response(200, ItemSchema)
     def get(self, store_id):
         return {"items": list(items.values())}
     
-    def post(self, store_id):
-        item_data = request.get_json()
-        if(
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(404, message="All fields are required")
-        
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemUpdateSchema)
+    def post(self,item_data):
         for item in items.values():
             if(item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]):
                 abort(404, message="Item already exists")
-                
-        if item_data["store_id"] not in stores:
-            abort(404, message="Store not found")
-        
+
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id} 
         items[item_id] = item
--- resources/store.py
+++ resources/store.py
@@ -4,12 +4,14 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -26,14 +28,13 @@ class Store(MethodView):
             
 @blp.route("/store")
 class StoreList(MethodView):
-    def get(self, store_id):
-        return {"stores": list(stores.values())}
+    @blp.response(200, StoreSchema(many=True))
+    def get(self, ):
+        return stores.values()
     
-    def post(self, store_id):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(404, message="All fields are required")
-            
+    @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if(store_data["name"] == store["name"]):
                 abort(400, message="Item already exists")
@@ -41,6 +42,6 @@ class StoreList(MethodView):
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id} 
         stores[store_id] = store
-        return store, 201
+        return store
     
     
